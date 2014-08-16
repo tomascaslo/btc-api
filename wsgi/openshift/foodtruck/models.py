9f7c5f52d353cbe1e3c120120d938e943de5d499
gchapa22 <gerardo.chapa.q@gmail.com>
2014-08-16 17:08:29 -0500

    Models and User app

--- wsgi/openshift/foodtruck/models.py
+++ wsgi/openshift/foodtruck/models.py
@@ -1,43 +1,86 @@
 from django.db import models
 
 class Foodtruck(models.Model):
-	name = models.CharField(max_length=40)
-	logo = models.ImageField()
-	picture = models.ImageField()
-	foodtype = models.ManyToManyField(Foodtype, verbose_name="foodtype list")
-	phone = models.IntegerField()
-	facebook = models.URLField()
-	twitter = models.URLField()
-	tumblr = models.URLField()
-	instagram = models.URLField()
-	webpage = models.URLField()
-	preorder_link = models.URLField()
-	menu = models.ManyToManyField(MenuItem, verbose_name="complete menu")
+	name = models.CharField(max_length=100)
+	logo = models.URLField(blank=True, null=True)
+	picture = models.URLField(blank=True, null=True)
+	foodtype = models.ManyToManyField(Foodtype, verbose_name="foodtype list",blank=True, null=True)
+	phone = models.IntegerField(blank=True, null=True)
+	# eat24, semeantoja, atumesa, etc
+	preorder_link = models.URLField(blank=True, null=True)
+	association = models.ForeignKey('Association',blank=True, null=True)
+	country = models.CharField(max_length=100) #https://developers.google.com/places/documentation/autocomplete
+	city = models.CharField(max_length=100) #https://developers.google.com/places/documentation/autocomplete
 
 	def __str__(self):
         return self.name
 
+class Location(models.Model):
+	foodtruck = models.ForeignKey('Foodtruck')
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	date = models.DateField()
+	opening_time = models.DateTimeField(blank=True, null=True)
+	closing_time = models.DateTimeField(blank=True, null=True)
+
+	def __str__(self):
+        return str(self.latitude) + ',' + str(self.longitude)
+
+class SocialMedia(models.Model):
+	TWITTER = 'TW'
+    FACEBOOK = 'FB'
+    TUMBLR = 'TB'
+    INSTAGRAM = 'IN'
+    PINTEREST = 'PN'
+    YOUTUBE = 'YT'
+    WEB = 'WB'
+    MEDIUM = 'MD'
+    VINE = 'VN'
+    FOURSQUARE = 'FQ'
+    SITE_CHOICES = (
+    	(FACEBOOK, 'Facebook'),
+        (TWITTER, 'Twitter'),
+	    (TUMBLR, 'Tumblr'),
+	    (INSTAGRAM, 'Instagram'),
+	    (PINTEREST, 'Pinterest'),
+	    (YOUTUBE, 'Youtube'),
+	    (WEB, 'Web Page'),
+	    (MEDIUM, 'Medium'),
+	    (VINE, 'Vine'),
+	    (FOURSQUARE, 'Foursquare')
+    )
+    site = models.CharField(max_length=2,
+                                      choices=SITE_CHOICES,
+                                      default=FACEBOOK)
+	url = models.URLField()
+	foodtruck = models.ForeignKey('Foodtruck')
+
+	def __str__(self):
+        return self.url
+
 class Foodtype(models.Model):
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=50)
 
 	def __str__(self):
         return self.name
 
 class MenuItem(models.Model):
-	name = models.CharField(max_length=40)
-	description = models.TextField()
-	price = models.DecimalField()
+	name = models.CharField(max_length=100)
+	description = models.TextField(blank=True, null=True)
+	price = models.DecimalField(blank=True, null=True)
+	picture = models.URLField(blank=True, null=True)
+	foodtruck = models.ForeignKey('Foodtruck')
 
 	def __str__(self):
         return self.name
 
 class Association(models.Model):
 	name = models.CharField(max_length=100)
-	country = CountryField() #https://developers.google.com/places/documentation/autocomplete
-	city = models.CharField() #https://developers.google.com/places/documentation/autocomplete
-	logo = models.ImageField()
-	verify = models.BooleanField(default=False)
-	passcode = models.CharField(max_length=40)
+	country = models.CharField(max_length=100) #https://developers.google.com/places/documentation/autocomplete
+	city = models.CharField(max_length=100) #https://developers.google.com/places/documentation/autocomplete
+	logo = models.ImageField(blank=True, null=True)
+	verified = models.BooleanField(default=False)
+	passcode = models.CharField(max_length=40,blank=True, null=True)
 
 	def __str__(self):
         return self.name
--- wsgi/openshift/payments/models.py
+++ wsgi/openshift/payments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users import foodtrucker
 
 # Create your models here.
+class Payment(models.Model):
+	date_time = models.DateTimeField()
+	amount = models.DecimalField()
+	conekta_user = models.CharField()
+	conekta_card = models.CharField()
+	conekta_transaction = models.CharField()
+	foodtrucker = models.ForeignKey('foodtrucker')
+
+    def __str__(self):
+        return str(self.amount)
